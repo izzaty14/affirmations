17a38e7cea87f346e92a3d73b4f38c0af9fe616c
Izzatyhusna <izzatyhusna02@gmail.com>
2022-02-27 15:51:22 +0800

    affirmations Izzaty

---
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -0,0 +1,22 @@
+package com.example.affirmations.data
+
+import com.example.affirmations.R
+import com.example.affirmations.model.affirmations
+
+
+class Datasource {
+    fun loadAffirmations(): List<affirmations> {
+        return listOf<affirmations>(
+            affirmations(R.string.affirmation1, R.drawable.image1),
+            affirmations(R.string.affirmation2, R.drawable.image2),
+            affirmations(R.string.affirmation3, R.drawable.image3),
+            affirmations(R.string.affirmation4, R.drawable.image4),
+            affirmations(R.string.affirmation5, R.drawable.image5),
+            affirmations(R.string.affirmation6, R.drawable.image6),
+            affirmations(R.string.affirmation7, R.drawable.image7),
+            affirmations(R.string.affirmation8, R.drawable.image8),
+            affirmations(R.string.affirmation9, R.drawable.image9),
+            affirmations(R.string.affirmation10, R.drawable.image10)
+        )
+    }
+}
---
+++ app/src/main/java/com/example/affirmations/model/affirmations.kt
@@ -0,0 +1,7 @@
+package com.example.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class affirmations(@StringRes val stringResourceId: Int,
+                        @DrawableRes val imageResourceId: Int)
